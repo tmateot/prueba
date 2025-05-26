e19308bed90ed83dd47136a112cc01be047a9958
tmateot <mateo85play@gmail.com>
2025-05-26 17:57:12 -0300

    hola

---
+++ script.js
@@ -0,0 +1,6 @@
+function Hola(){
+    const numero1=parseInt(document.getElementById("num1").value)
+    const numero2=parseInt(document.getElementById("num2").value)
+    alert(numero1+numero2)
+
+}
